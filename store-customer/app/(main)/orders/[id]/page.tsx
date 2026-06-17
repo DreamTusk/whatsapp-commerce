715ddced819ebc918013be5a1ff39ad11ea8435b
venkatesh08-DreamTusk <venkatesh.r@dreamtusk.com>
2026-06-17 17:51:36 +0530

    unused files deleted

--- store-customer/app/(main)/orders/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import OrderDetailClient from './order-detail-client'
-
-export default function OrderDetailPage() {
-  return (
-    <main className="min-h-screen" style={{ backgroundColor: '#F8F9FA' }}>
-      <OrderDetailClient />
-    </main>
-  )
-}
